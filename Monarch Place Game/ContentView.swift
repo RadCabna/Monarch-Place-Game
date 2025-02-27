d5b20369d2df7dce8a9be01bac6c599b141a1c56
Radcabna <radcabna@gmail.com>
2025-02-27 20:11:40 +0300

    Initial Commit

---
+++ Monarch Place Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Monarch Place Game
+//
+//  Created by Алкександр Степанов on 27.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Monarch Place Game/Monarch_Place_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Monarch_Place_GameApp.swift
+//  Monarch Place Game
+//
+//  Created by Алкександр Степанов on 27.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Monarch_Place_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
